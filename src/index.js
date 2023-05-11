c1ae6981668d1c10e3de3c0f0b02c49d1ce2ecf7
TrungDang <trungdangdev06@gmail.com>
2023-05-11 22:33:11 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+console.log("PATH", path.join(__dirname, "resources", "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port);
